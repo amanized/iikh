9f17d01a8f6761c1052e336e7926bd93430db1f8
yjhykim@gmail.com <yjhykim@gmail.com@f8c22881-63dd-3596-f1ba-8bac45531b11>
2010-09-20 10:10:01 +0000

    RecipeDTO 맴버변수정의 함수정의

---
+++ trunk/IIKH team2/IIKH team2/RecipeDTO.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class RecipeDTO{
+
+private:
+	string name;
+	string meterial;
+	string detail;
+	string summary;
+
+public:
+	
+	void setName(string name);
+	string getName();
+	
+	void setMaterial(string material);
+	string getMaterial();
+
+	void setDetail(string detail);
+	string getDetail();
+
+	void setSummary(string summary);
+	string getSummary();
+	
+
+
+}
